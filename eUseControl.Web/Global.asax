41ba329f0c6bcdda1b4b33b41bfa9c1fad746fc0
sasha442 <62384640+sasha442@users.noreply.github.com>
2020-05-23 12:41:52 +0300

    Добавьте файлы проекта.

---
+++ eUseControl.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eUseControl.Web.MvcApplication" Language="C#" %>
---
+++ eUseControl.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eUseControl.Web.Startup))]
+namespace eUseControl.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
